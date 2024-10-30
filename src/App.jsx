1a712f4b570c954ceff1b00b8c7f02cb5d5bf47a
navneet <navneet.work9@gmail.com>
2024-10-31 01:55:14 +0530

    Protected routes Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,82 @@
-import React from 'react'
-import { Button } from './components/ui/button'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import AppLayout from './layouts/app-layout'
-import LandingPage from './pages/landing'
-import Onboarding from './pages/Onboarding'
-import JobPage from './pages/Job'
-import Postjob from './pages/Postjob'
-import SavedJobs from './pages/saved-jobs'
-import MyJobs from './pages/my-jobs'
-import Joblisting from './pages/Joblisting'
-import { ThemeProvider } from './components/ui/theme-provider'
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AppLayout from './layouts/app-layout';
+import LandingPage from './pages/landing';
+import Onboarding from './pages/Onboarding';
+import JobPage from './pages/Job';
+import Postjob from './pages/Postjob';
+import SavedJobs from './pages/saved-jobs';
+import MyJobs from './pages/my-jobs';
+import Joblisting from './pages/Joblisting';
+import { ThemeProvider } from './components/ui/theme-provider';
+import ProtectedRoute from './components/protected-routes';
 
 const router = createBrowserRouter([
-  { 
-    element : <AppLayout></AppLayout>,
-    children : [ 
-      { 
-        path : '/', 
-        element:<LandingPage/>
-      }, 
-      { 
-        path : '/onboarding', 
-        element:<Onboarding/>
-      }, 
-      { 
-        path : '/jobs', 
-        element:<Joblisting/>
-      }, 
-      { 
-        path : '/job/:id', 
-        element: <JobPage/>
-      }, 
-      { 
-        path : '/post-job', 
-        element:<Postjob/>
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: (
+         
+            <LandingPage />
+      
+        ),
       },
-      { 
-        path : '/saved-jobs', 
-        element:<SavedJobs/>
-      }, 
-      { 
-        path : '/my-jobs', 
-        element: <MyJobs/>
-      }
-    ]
-  }
-])
+      {
+        path: '/onboarding',
+        element: <Onboarding />,
+      },
+      {
+        path: '/jobs',
+        element: (
+          <ProtectedRoute>
+            <Joblisting />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/job/:id',
+        element: (
+          <ProtectedRoute>
+            <JobPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/post-job',
+        element: (
+          <ProtectedRoute>
+            <Postjob />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/saved-jobs',
+        element: (
+          <ProtectedRoute>
+            <SavedJobs />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/my-jobs',
+        element: (
+          <ProtectedRoute>
+            <MyJobs />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
+
 const App = () => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-  <RouterProvider router={ router } />
-  </ThemeProvider>
-    
-  )
-}
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/protected-routes.jsx
@@ -0,0 +1,14 @@
+import { useUser } from '@clerk/clerk-react'
+import React from 'react'
+import { Navigate, useLocation } from 'react-router-dom'
+
+const ProtectedRoute = ({children}) => {
+    const {isSignedIn , user , isLoaded} = useUser() ; 
+    const {pathname }  = useLocation() ; 
+    if (isLoaded && !isSignedIn && isSignedIn !== undefined)  {
+        return <Navigate to ='/?sign-in=true'></Navigate>
+    }
+    return children ; 
+}
+
+export default ProtectedRoute
